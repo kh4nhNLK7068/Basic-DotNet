c9ad806f92b5a99f7dca47e832b270192d6af06b
kh4nhNLK7068 <khanhnguyennlk4198@gmail.com>
2026-05-06 16:01:32 +0700

    Exception-1

---
+++ Exception/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ExceptionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+                int result = 10 / n;
+
+                Console.WriteLine($"Result: {result}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message} \n" + ex.ToString());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("This block will always execute.");
+            }
+        }
+    }
+}
