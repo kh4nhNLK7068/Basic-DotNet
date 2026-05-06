16999493173010894a6f4fc351f2aaefa6cfe972
kh4nhNLK7068 <khanhnguyennlk4198@gmail.com>
2026-05-06 11:32:21 +0700

    Method

---
+++ Method/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci(2);
+        }
+
+        static void SwapNumbers(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void Fibonacci(int n)
+        {
+            if (n <= 0)
+            {
+                Console.WriteLine("Please enter a positive integer.");
+                return;
+            }
+            int a = 0, b = 1, c;
+            Console.Write("Fibonacci Series: ");
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(a + " ");
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            Console.WriteLine();
+        }
+    }
+}
