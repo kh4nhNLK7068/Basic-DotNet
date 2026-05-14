71dece3a69704669465f4316650a0578019040cb
Nguyễn Lê Khanh <khanhnl@sags.vn>
2026-05-14 16:58:37 +0700

    Lambda: expression and statement lambda (1)

---
+++ Lambda/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Lambda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // expression lambda
+            Func<int, int, string> sum = (int x, int y) => (x + y).ToString();
+            Console.WriteLine(sum(5, 10)); // Output: 15
+
+            // statement lambda
+            var upper = (string s) => Console.WriteLine(s.ToUpper());
+            upper("hello world"); // Output: HELLO WORLD
+
+            Action<string> greet = (string name) =>
+            {
+                string greeting = $"Hello, {name}!";
+                Console.WriteLine(greeting);
+            };
+            greet("Alice"); // Output: Hello, Alice!
+        }
+    }
+}
